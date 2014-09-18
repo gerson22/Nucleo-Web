dd8758c7d5d9f4a3cadb1c34914c4c5c41b7f0cf
Yozki <gus_carrillo@live.com>
2014-09-18 17:14:04 -0500

    Inicio API plataforma

---
+++ API/clases/Database.php
@@ -0,0 +1,52 @@
+<?php
+
+class Database
+{
+    private static $db_host        = "localhost";
+    private static $db_user        = "root";
+    private static $db_password    = "root";
+    private static $db_database    = "prevenla_meze";
+
+    static function select($query)
+    {
+        $mysqli = new mysqli(self::$db_host, self::$db_user, self::$db_password, self::$db_database);
+        echo $mysqli->connect_error;
+        $result = $mysqli->query($query);
+        if($result)
+        {
+            $rows = self::resultToArray($result);
+            $result->free();
+            $mysqli->close();
+            return $rows;
+        }
+        return null;
+    }
+
+    static function insert($query)
+    {
+        $mysqli = new mysqli(self::$db_host, self::$db_user, self::$db_password, self::$db_database);
+        $mysqli->multi_query($query);
+        $insert_id = $mysqli->insert_id;
+        $mysqli->close();
+        return $insert_id;
+    }
+
+    static function update($query)
+    {
+        $mysqli = new mysqli(self::$db_host, self::$db_user, self::$db_password, self::$db_database);
+        $success = $mysqli->query($query);
+        $mysqli->close();
+        return $success;
+    }
+
+    static function resultToArray($result)
+    {
+        $rows = array();
+        /** @noinspection PhpUndefinedMethodInspection */
+        while($row = $result->fetch_assoc())
+        {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+}
