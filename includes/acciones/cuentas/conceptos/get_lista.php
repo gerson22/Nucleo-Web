de4c40c17e6d0473ade1f0ea8f0fedb472fb73e1
Yozki <gus_carrillo@live.com>
2014-12-30 21:37:24 -0600

    Quité el utf8_decode de get_lista.php de cuentas (no jalaba en producción)

--- includes/acciones/cuentas/conceptos/get_lista.php
+++ includes/acciones/cuentas/conceptos/get_lista.php
@@ -20,7 +20,7 @@ if(is_array($conceptos))
     foreach($conceptos as $concepto)
     {
         $temp = array();
-        array_push($temp, utf8_decode($concepto['concepto']));
+        array_push($temp, $concepto['concepto']);
         $link = '<a href="/admin/cuentas/conceptos/detalles.php?id_concepto='.$concepto['id_concepto'].'" >
             <img src="/media/iconos/icon_profile.png" alt="X" /></a>';
         array_push($temp, $link);
