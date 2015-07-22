ac2658de0a4419e12485da52abb7efed2989ecf5
Gustavo Carrillo <gustavocarrillo@nop.al>
2015-07-21 20:20:35 -0500

    Impresión de información de recibo.

--- API/modelos/PagoModelo.php
+++ API/modelos/PagoModelo.php
@@ -15,7 +15,22 @@ class PagoModelo
 
     public static function getPago($pagoID)
     {
-        $query = "SELECT * FROM cuentas_pago WHERE id_pago = $pagoID LIMIT 1";
+        $query = 'SELECT recibo, DATE_FORMAT(fecha,"%d-%m-%Y") AS fecha, CONCAT(persona_pago.nombres, " ", persona_pago.apellido_paterno, " ", persona_pago.apellido_materno) AS persona,
+            curp_persona.CURP AS CURP_persona,
+             CONCAT(calle, " ", numero, " ", colonia.nombre, " ", CP) AS direccion, "Torreón" AS ciudad,
+            cuentas_pago.monto AS cantidad, concepto, forma_pago, cuentas_cuenta.id_cuenta,
+             CONCAT(persona.nombres, " ", persona.apellido_paterno, " ", persona.apellido_materno) AS alumno, curp_alumno.CURP AS CURP_alumno
+            FROM cuentas_pago
+            JOIN cuentas_cuenta ON cuentas_cuenta.id_cuenta = cuentas_pago.id_cuenta
+            JOIN cuentas_forma_pago ON cuentas_forma_pago.id_forma_pago = cuentas_pago.id_forma_pago
+            JOIN cuentas_concepto ON cuentas_concepto.id_concepto = cuentas_cuenta.id_concepto
+            JOIN persona ON cuentas_cuenta.id_persona = persona.id_persona
+            LEFT JOIN curp AS curp_alumno ON curp_alumno.id_persona = persona.id_persona
+            LEFT JOIN persona_direccion ON persona_direccion.id_persona = persona.id_persona
+            LEFT JOIN colonia ON colonia.id_colonia = persona_direccion.id_colonia
+            LEFT JOIN persona AS persona_pago ON persona_pago.id_persona = cuentas_pago.id_persona
+            LEFT JOIN curp AS curp_persona ON curp_persona.id_persona = persona_pago.id_persona
+            WHERE cuentas_pago.id_pago = '.$pagoID.' LIMIT 1';
         return APIDatabase::select($query);
     }
 }
