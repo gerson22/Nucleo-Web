504c80143d9c0e8f081c7c90f67fd794f98a0500
gerson22 <gerson.isaias.rivera@gmail.com>
2015-09-01 14:23:08 -0500

    Diseños docente y alumno
    se cambiaron estructuras en index de alumno docente y sus respectivos
    archivos

--- includes/menu_alumno.php
+++ includes/menu_alumno.php
@@ -1,8 +1,22 @@
-<nav class="menu_strip" id="menu_administrador">
-    <ul id="menu_strip_lista">
-        <li class="li_menu" ><a href="/index.php" >Inicio</a></li>
-        <li class="li_menu" >
-            <a href="/calificaciones.php" >Calificaciones</a></li>
-        </li>
-    </ul>
-</nav>
+		<!--- Contenedor de Menu lateral y de contenido ---->
+		<div class="container-fluid">
+				<!----Menu Lateral --->
+				<div class="col-sm-2 col-md-2 col-lg-2 sidebar  collapse in" id="menus">
+					<ul class="nav nav-sidebar">
+						<li>
+							<a href="#user" data-toggle="collapse" data-parent="#MainMenu" class="letter22"><img src="/img/user.png" height="48" style="margin-top:10px;"><br><label class="letter22"><?php if(isset($_SESSION['nombres'])) echo $_SESSION['nombres'] ; ?></label></a>
+						</li>
+							<div class="collapse" id="user">
+								<li><a href="#" class="list-group-item letter3">Configuraciones</a></li>
+								<li><a href="/includes/logout.php" class="list-group-item letter3">Salir</a></li>
+							</div>
+						<li>
+							<a id="pag_home" class="letter22" href="/index.php"><span class="glyphicon glyphicon-home ico"></span><b> INICIO</b></a>
+						</li>
+						<li>
+							<a id="pag_home" class="letter3" href="/calificaciones.php"><span class="glyphicon glyphicon-education ico"></span> Calificaciones</a>
+						</li> 
+						
+					</ul>
+				</div>
+</div>
--- maestros/clase.php
+++ maestros/clase.php
@@ -15,6 +15,11 @@ if(is_null($clase->id_clase)){ header('Location: /index.php'); exit; }
     <head>
         <meta charset="utf-8" />
         <title>Sistema Integral Meze - Clase</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="../js/jquery.js" type="text/javascript"></script>
+		<script src="../js/bootstrap.js" type="text/javascript"></script>
+		<script src="../plugins/assets/js/appear.min.js" type="text/javascript"></script>
+		<script src="../plugins/assets/js/animations.js" type="text/javascript"></script>
         <link rel="stylesheet" href="../../estilo/general.css" />
         <link rel="stylesheet" href="../../estilo/index.css" />
         <link rel="stylesheet" href="../../estilo/jquery.dataTables.css" />
@@ -44,10 +49,9 @@ if(is_null($clase->id_clase)){ header('Location: /index.php'); exit; }
         </script>
     </head>
     <body>
-        <div id="wrapper">
             <?php include("../includes/header.php"); ?>
-            <div id="content">
-
+            <div id="principal" class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2" style="margin-top:20px;">
+                <div id="area_trabajo">
                 <table id="tabla_alumnos">
                     <thead>
                         <tr>
