42bfadb796229bce5b5a775686863c95567c329c
Gustavo Carrillo <gus_carrillo@live.com>
2015-09-10 22:30:46 -0500

    Llamas API Tareas

---
+++ API/controladores/tareasControlador.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yozki
+ * Date: 20/07/2015
+ * Time: 08:32 PM
+ */
+
+class tareasControlador
+{
+    public function procesar($metodo, $verbo, $argumentos)
+    {
+        switch($metodo)
+        {
+            case "GET":
+                $persona = PersonaModelo::getPersonaPorCredenciales($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+                if($persona->tipo_persona = 1)
+                {
+                    $alumno = new AlumnoModelo($persona->id_persona);
+                    return $alumno->getTareas();
+                }
+                else if($persona->tipo_persona = 3)
+                {
+                    return TareaModelo::getLista();
+                }
+                echo "402";
+                return 403;
+            case "POST":
+                return $this->asignarTarea();
+                break;
+            default:
+                break;
+        }
+        return 404;
+    }
+
+    public function asignarTarea()
+    {
+        TareaModelo::addTarea($_POST['id_clase'], $_POST['descripcion'], $_POST['fecha_entrega']);
+    }
+}
